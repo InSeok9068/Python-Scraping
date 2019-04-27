52f6d2700a7e429eb6d9794444f6dc29a332d62c
Lee In Seok <dlstjr9068@naver.com>
2019-04-27 11:24:39 +0900

    Chapter1 Finish

--- scrapetest.py
+++ scrapetest.py
@@ -1,6 +1,22 @@
 from urllib.request import urlopen
 from urllib.request import HTTPError
 from bs4 import BeautifulSoup
-html = urlopen("http://pythonscraping.com/pages/page1.html")
-bsObj = BeautifulSoup(html.read(), "html.parser")
-print(bsObj.h1)  
+
+def getTitile(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bsObj = BeautifulSoup(html.read(), "html.parser")
+        title = bsObj.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+title = getTitile("http://pythonscraping.com/pages/page1.html")
+
+if title == None:
+    print("Title could not be found")
+else:
+    print(title)
