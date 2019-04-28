aa2c1e754c5061735293408fd8a7e4e67f6d5a77
Lee In Seok <dlstjr9068@naver.com>
2019-04-28 14:08:14 +0900

    워드 읽기 수정

--- scrapetest21.py
+++ scrapetest21.py
@@ -11,4 +11,14 @@ xml_content = document.read('word/document.xml')
 wordObj = BeautifulSoup(xml_content.decode("utf-8"), "html.parser")
 textStrings = wordObj.findAll("w:t")
 for textElem in textStrings:
+    closeTag = ""
+    try:
+        style = textElem.parent.previousSibling.find("w:pstyle")
+        if style is not None and style["w:val"] == "Title" :
+            print("<h1>")
+            closeTag = "</h1>"
+    except AttributeError:
+    # 출력할 태그가 없습니다.
+        pass
     print(textElem.text)
+    print(closeTag)
